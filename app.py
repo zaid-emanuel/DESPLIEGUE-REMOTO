b3aba830633ab4cda494e6d12ceeca7c46c6c7a5
Zaid <233111046@upmh.edu.mx>
2026-08-11 15:07:34 -0600

    Cambios para desplegar el servidor

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 import requests
-
+import os
 app = Flask(__name__)
 
 API_URL = "https://randomuser.me/api/"
@@ -37,4 +37,5 @@ def generar():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
